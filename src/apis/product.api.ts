81398f95341333251a0d26ff553cd685dfd93fc5
Ric <ricpham1902@gmail.com>
2024-08-02 11:19:14 +0700

    revert: remove new syntax call super

--- src/apis/product.api.ts
+++ src/apis/product.api.ts
@@ -1,6 +1,7 @@
 import { BaseApi } from '@/apis'
 import { IProductItem } from '@/interfaces'
 import { ParamsType } from '@/types'
+import { axiosClient } from '@/utils'
 
 class ProductApi extends BaseApi<IProductItem> {
   constructor() {
@@ -8,16 +9,18 @@ class ProductApi extends BaseApi<IProductItem> {
   }
 
   getMyProduct(params?: ParamsType) {
-    this.endpoint = `/${this.key}/my_product`
-    return super.getListPagination(params)
+    return axiosClient.get(`${this.endpoint}/my_product`, {
+      params
+    })
   }
   getKeyForMyProduct(params?: ParamsType) {
     return [this.key, 'getListPagination', 'my_product', params]
   }
 
   getPublic(params?: ParamsType) {
-    this.endpoint = `/${this.key}/public`
-    return super.getListPagination(params)
+    return axiosClient.get(`${this.endpoint}/public`, {
+      params
+    })
   }
   getKeyForPublic(params?: ParamsType) {
     return [this.key, 'getListPagination', 'public', params]
